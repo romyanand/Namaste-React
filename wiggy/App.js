726a338f3815855c5c2f44cced6c4962ab5d4764
romyanand <romyaanand@gmail.com>
2023-09-19 16:33:59 +0530

    imported react, react-dom and installed parcel

--- wiggy/App.js
+++ wiggy/App.js
@@ -1,18 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const appLayout = 
-React.createElement("div", {id: "appLayout"}, 
-
-[React.createElement("h1", {id: "heading"}, "This is a heading"),
-
-React.createElement("h2", {id: "sub-heading"}, "This is a sub heading"),
-
-React.createElement("div", {id: "content-box"},
-
- [React.createElement("p", {id: "par-1"}, "This is a paragraph and it's direct parent is a `div` of `id:content` box which in itself is child of a `div` of `id:appLayout` and sibling of heading and subheading"),
-
-  React.createElement("P", {id:"par-2"}, "This is also a paragraph inside content-box so it's sibling of the above paragraph")])])
+const App = () => {
+  return(
+    <h1>Hello There!</h1>
+  )
+}
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(appLayout)
+root.render(<App />)

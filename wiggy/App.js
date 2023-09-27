9b0a81aae757a0ce95c25684082a2907dcf2f8e1
romyanand <romyaanand@gmail.com>
2023-09-27 20:09:06 +0530

    03-created nested header element using React.createElement

--- wiggy/App.js
+++ wiggy/App.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const App = () => {
-  return(
-    <h1>Hello There!</h1>
-  )
-}
+const header = React.createElement("div", {className: "title"}, [React.createElement("h1", {}, "This is an h1 heading"), React.createElement("h2", {}, "This is an h2 heading"), React.createElement("h3", {}, "This is an h3 heading")])
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(<App />)
+root.render(header)

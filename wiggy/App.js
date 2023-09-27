4a13d54a231af74e5a05b8955296ca78137eeea4
romyanand <romyaanand@gmail.com>
2023-09-27 21:03:00 +0530

    made a functional component of the same JSX

--- wiggy/App.js
+++ wiggy/App.js
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 
-const header = <div className='title'>
-  <h1>This is an h1 heading</h1>
-  <h2>This is an h2 heading</h2>
-  <h3>This is an h3 heading</h3>
-</div>
+const Header = () => {
+return (
+    <div className='title'>
+      <h1>This is an h1 heading</h1>
+      <h2>This is an h2 heading</h2>
+      <h3>This is an h3 heading</h3>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-root.render(header)
+root.render(<Header />)
